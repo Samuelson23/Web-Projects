a9918145ae0dcb8c0525b63a25626f95b8c917f2
Samuelson23 <3samuelmg@gmail.com>
2023-05-01 18:48:25 +0200

    api rick y morty

--- rickYmorty-Api/main.js
+++ rickYmorty-Api/main.js
@@ -1,7 +1,7 @@
 import "./style.css";
-import { printTemplate as Header } from "./components/header/header";
+import { printTemplate as Header } from "./components/header/Header";
 import { printTemplate as Home } from "./components/Home/Home";
-import { printTemplate as Footer } from "./components/footer/footer";
+import { printTemplate as Footer } from "./components/Footer/Footer";
 
 Header();
 Home();
